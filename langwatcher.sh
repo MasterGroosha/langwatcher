89dc46cda47709b35516a2d317b580f89ec8e7e3
Evgeny Petrov <groosha@protonmail.com>
2019-08-07 16:37:50 +0000

    Updated langwatcher.sh

--- langwatcher.sh
+++ langwatcher.sh
@@ -4,7 +4,8 @@ git pull
 cd /home/botfather/langwatcher/python
 bin/python watcher.py
 cd ../langwatcher-site
-hugo
+/usr/local/bin/hugo
+sleep 2s
 cd ..
 git add .
 git commit -m "Autocommit $(date +%Y%m%d-%H:%M) GMT"
